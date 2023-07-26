beb84b15e9e95f7df61beb42ab8e6cee2dfffc08
Samin Yaser <saminyaserwork@gmail.com>
2023-07-26 07:32:42 +0600

    removed unused import

--- app/Filament/Resources/SalaryResource/Pages/ManageSalaries.php
+++ app/Filament/Resources/SalaryResource/Pages/ManageSalaries.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\SalaryResource\Pages;
 
 use App\Filament\Resources\SalaryResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageSalaries extends ManageRecords
