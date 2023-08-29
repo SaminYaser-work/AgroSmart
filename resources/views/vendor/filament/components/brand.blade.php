e3d5c839f63576bc83282c38e386749071df6154
Samin Yaser <saminyaserwork@gmail.com>
2023-08-29 07:09:14 +0600

    branding and user change

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('logo/AgroSmart-logos/AgroSmart-logos_transparent.png') }}" alt="Logo" style="height: 300px;">
+<img src="{{ asset('logo/AgroSmart-logos/AgroSmart-logos_transparent.png') }}" alt="Logo" style="width: 100%; max-height: 300px; object-fit: cover;">
